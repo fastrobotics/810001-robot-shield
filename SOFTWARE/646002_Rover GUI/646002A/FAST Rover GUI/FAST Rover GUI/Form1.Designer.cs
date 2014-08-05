c2d4269450f5bbe378b9839071307f16b68c2045
davidgitz <davidgitz@gmail.com>
2014-08-05 17:03:14 -0500

    Added datasheet.

--- SOFTWARE/646002_Rover GUI/646002A/FAST Rover GUI/FAST Rover GUI/Form1.Designer.cs
+++ SOFTWARE/646002_Rover GUI/646002A/FAST Rover GUI/FAST Rover GUI/Form1.Designer.cs
@@ -283,6 +283,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(1313, 386);
             this.Controls.Add(this.scrollSteer);
             this.Controls.Add(this.scrollThrottle);
--- SOFTWARE/646003_Robot Shield Demo/646003/README.md
+++
@@ -1,1 +0,0 @@
-Unfortunately, due to insufficient lab space, I am no longer able to continue development on this project.  If you have any questions about this project please email me at davidgitz at gmail.com
